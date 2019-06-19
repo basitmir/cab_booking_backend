53298c850da97a1ee3bc2a6147e69a5a0e975473
Basit Mir <iammirbasit@gmail.com>
2019-06-19 16:36:06 +0530

    env file temp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+// use Illuminate\Http\Resources\Json\Resource;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Resource::withoutWrapping();
         Schema::defaultStringLength(191);
     }
 }
